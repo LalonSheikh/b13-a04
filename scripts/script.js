858a3aceaefeb0a32a5436c79bb38e10dd71af09
LalonSheikh <abusalekmdlalonsheikh@gmail.com>
2026-02-24 22:14:06 +0600

    delete functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -47,3 +47,44 @@ for (const card of arrays) {
     totalNumber.innerText = Number(totalNumber.innerText) - 1;
   });
 }
+//  delete function added
+// const totalNumber = document.getElementById("total-job");
+// const interviewNumber = document.getElementById("interview-number");
+// const rejectedNumber = document.getElementById("rejected-number");
+// const selectedCardNumber = document.getElementById("selected-card-number");
+
+// const cardsContainer = document.getElementById("card-container");
+
+function updateSelectedCount() {
+  const remaining = cardsContainer.children.length;
+  selectedCardNumber.innerText = remaining;
+}
+
+const allCards = document.querySelectorAll(".delete-btn");
+
+for (let i = 0; i < allCards.length; i++) {
+  allCards[i].addEventListener("click", function () {
+    const card = this.closest("div[id^='card-']");
+    const statusSpan = card.querySelector("span");
+    const status = statusSpan.innerText;
+
+    // Adjust dashboard count based on status
+    if (status === "NOT APPLIED") {
+      totalNumber.innerText = Number(totalNumber.innerText) - 1;
+    }
+
+    if (status === "INTERVIEW") {
+      interviewNumber.innerText = Number(interviewNumber.innerText) - 1;
+    }
+
+    if (status === "REJECTED") {
+      rejectedNumber.innerText = Number(rejectedNumber.innerText) - 1;
+    }
+
+    // Remove card
+    card.remove();
+
+    // Update remaining jobs counter
+    updateSelectedCount();
+  });
+}
