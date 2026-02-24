43d8597642edb1dd482706b7b84d2f27159ec1ef
LalonSheikh <abusalekmdlalonsheikh@gmail.com>
2026-02-24 21:16:53 +0600

    interview functionality added

---
+++ scripts/common.js
@@ -0,0 +1,30 @@
+// const cardsContainer = document.getElementById("card-container");
+// const interviewNumber = document.getElementById("interview-number");
+// const selectedInterview = document.getElementById("selected-interview");
+// const arrays = Array.from(cardsContainer);
+// document.getElementById("interview-btn").addEventListener("click", function () {
+//   console.log("interview button clicked");
+//   const cardsArray = Array.from(cardsContainer.children);
+//   //   const allCards = cardsContainer.children;
+//   console.log(typeof allCards);
+//   for (const card in allCards) {
+//     console.log("card:", card);
+//   }
+
+//   // Count total cards
+//   const totalCards = allCards.length;
+
+//   if (totalCards) console.log("Total cards:", totalCards, typeof totalCards);
+
+//   const totalJob = document.getElementById("total-job");
+//   const totalJobNumber = Number(totalJob.innerText);
+
+//   console.log(totalJobNumber);
+
+//   const card = document.getElementById("card");
+
+//   const copyCard = card.cloneNode(true);
+//   const selectedInterview = document.getElementById("selected-interview");
+
+//   selectedInterview.appendChild(copyCard);
+// });
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,24 @@
-document.getElementById("interview-btn").addEventListener("click", function () {
-  console.log("interview button clicked");
-});
+const cardsContainer = document.getElementById("card-container");
+
+const arrays = Array.from(cardsContainer.children);
+
+const interviewNumber = document.getElementById("interview-number");
+const selectedInterview = document.getElementById("selected-interview");
+
+for (const card of arrays) {
+  const interviewBtn = card.querySelector(".interview-btn");
+  const statusSpan = card.querySelector("span");
+
+  interviewBtn.addEventListener("click", function () {
+
+
+    statusSpan.innerText = "INTERVIEW";
+    statusSpan.className =
+      "bg-green-100 p-2 font-bold text-green-900 rounded-xl";
+
+    selectedInterview.appendChild(card);
+
+    const currentCount = Number(interviewNumber.innerText);
+    interviewNumber.innerText = currentCount + 1;
+  });
+}
